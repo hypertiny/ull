22c30644915a004679d036fb71212151c4b3e9bd
Kilian McMahon <kil@kilmc.com>
2013-10-30 18:22:59 +0000

    Check if user is typing and loop until half a second has passed between keypresses

--- scripts/dude.js
+++ scripts/dude.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
 
   var frame = 41.666667;
+  var timer;
 
-  $( ".register-name, .register-email" ).keypress(function() {
+  $( ".register-name, .register-email" ).one("keypress", function() {
     $(".volunteer-image").attr("src", "/images/dude/02-dude.gif");
     setTimeout(function() {
       $(".volunteer-image").attr("src", "/images/dude/03-dude.gif")
     }, frame*3);
   });
-  $( ".register-name, .register-email" ).keyup(function() {
-    setTimeout(function() {
-      $(".volunteer-image").attr("src", "/images/dude/04-dude.gif");
+  $( ".register-name, .register-email" ).on("keyup", function() {
+    clearInterval(timer);  //clear any interval on key up
+    timer = setTimeout(function() { 
       setTimeout(function() {
-        $(".volunteer-image").attr("src", "/images/dude/01-dude.gif")
-      }, frame*4);
-    }, frame*12);
+        $(".volunteer-image").attr("src", "/images/dude/04-dude.gif");
+        setTimeout(function() {
+          $(".volunteer-image").attr("src", "/images/dude/01-dude.gif")
+        }, frame*4);
+      }, frame*12);
+    }, 500);
   });
 });
+
+
+
+
+
+
+
+
