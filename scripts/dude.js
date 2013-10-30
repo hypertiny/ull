7c9ef0b72bac479fb0819794f351e2d821967866
Kilian McMahon <kil@kilmc.com>
2013-10-30 18:09:00 +0000

    Add frame variable for easier calculation

--- scripts/dude.js
+++ scripts/dude.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
+
+  var frame = 41.666667;
+
   $( ".register-name, .register-email" ).keypress(function() {
     $(".volunteer-image").attr("src", "/images/dude/02-dude.gif");
     setTimeout(function() {
       $(".volunteer-image").attr("src", "/images/dude/03-dude.gif")
-    }, 125);
+    }, frame*3);
   });
   $( ".register-name, .register-email" ).keyup(function() {
     setTimeout(function() {
       $(".volunteer-image").attr("src", "/images/dude/04-dude.gif");
       setTimeout(function() {
         $(".volunteer-image").attr("src", "/images/dude/01-dude.gif")
-      }, 167);
-    }, 500);
+      }, frame*4);
+    }, frame*12);
   });
 });
