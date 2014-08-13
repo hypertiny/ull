70e662d34281e23ed5480e938423b335001b6899
Paul Campbell <paul@rslw.com>
2014-08-13 10:48:38 +0100

    2015

--- javascripts/register_interest.js
+++ javascripts/register_interest.js
@@ -9,7 +9,7 @@ jQuery(function(){
     email = $('#register-email').val()
     if($.trim(name) != '' && $.trim(email) != '')
     {
-      url = ["https://ti.to/ull/2014/interested_users/subscribe.json?&interested_user[email]=", email , "&interested_user[name]=", name, "&callback=?"].join('')
+      url = ["https://ti.to/ull/2015/interested_users/subscribe.json?&interested_user[email]=", email , "&interested_user[name]=", name, "&callback=?"].join('')
       $.getJSON(url, null, function(data){})
       width = $('.register-interest-form').width()
       height = $('.register-interest-form').height()
