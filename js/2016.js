0641f415929386e53bfe33680bed39a88d91702d
Paul Campbell <paul@rslw.com>
2016-02-05 23:28:09 +0000

    tweak the animation

--- js/2016.js
+++ js/2016.js
@@ -1,10 +1,12 @@
 jQuery(function(){
   $(document).on('click', '[data-behavior=watch]', function(){
     $('#buttons').fadeOut(function(){
-      $('.vertical_space_3').animate({height: 3})
       $('#border').animate({maxWidth: ($(window).width() - 20)}, function(){
-        $('#video').tab('show')
-        Wistia.api('8axqrasvy5').play()
+        $('.vertical_space_3').animate({height: 5})
+        $('#border').animate({height: $(window).height() - 30}, function(){
+          $('#video').tab('show')
+          Wistia.api('8axqrasvy5').play()
+        })
       })
     })
     return false
