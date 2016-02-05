04a4bd4a4cab816485d4e2d1cb92315349c0467e
Paul Campbell <paul@rslw.com>
2016-02-05 23:39:12 +0000

    centre things

--- js/2016.js
+++ js/2016.js
@@ -2,8 +2,10 @@ jQuery(function(){
   $(document).on('click', '[data-behavior=watch]', function(){
     $('#buttons').fadeOut(function(){
       $('#border').animate({maxWidth: ($(window).width() - 20)}, function(){
-        $('.vertical_space_3').animate({height: 5})
-        $('#border').animate({height: $(window).height() - 30}, function(){
+        newHeight = ($(window).height() - $(window).width() / 1.77) / 2 / 3
+        $('.vertical_space_3').animate({height: newHeight})
+        $('.box-frame-2').animate({height: ($(window).width() / 1.77) - 10, maxHeight: ($(window).width() / 1.77) - 10})
+        $('#border').animate({height: $(window).width() / 1.77}, function(){
           $('#video').tab('show')
           Wistia.api('8axqrasvy5').play()
         })
